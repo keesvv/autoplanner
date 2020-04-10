1ba2bb170e189df9fba6c52bfdc544cbdc66b4b2
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-04-10 23:57:10 +0200

    Add support for remembering creds

--- src/auth.py
+++ src/auth.py
@@ -1,19 +1,29 @@
 import config
 import json
 import sys
+import pickle
+from os import path
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 
 class AuthProvider:
     def __init__(self, school_url):
-        self.driver = webdriver.Chrome()
         self.school_url = school_url
 
-    """
-    Authenticate the user
-    """
-    def authenticate(self):
+    def __load_session(self):
+        session_file = open('.session', 'rb')
+        content = session_file.read()
+        session_file.close()
+        self.session = pickle.loads(content)
+
+    def __write_session(self):
+        session_file = open('.session', 'wb')
+        pickle.dump(self.session, session_file)
+        session_file.close()
+
+    def __login(self):
+        self.driver = webdriver.Chrome()
         self.driver.get('https://' + self.school_url)
         wait = WebDriverWait(self.driver, config.TIMEOUT)
 
@@ -28,13 +38,23 @@ class AuthProvider:
             self.driver.quit()
             sys.exit(1)
 
-        item = f'oidc.user:https://accounts.magister.net:M6-{self.school_url}'
-        raw_session = self.driver.execute_script(f"return window.sessionStorage.getItem('{item}')")
-
-        # Parse the raw session
-        session = json.loads(raw_session)
-
-        self.access_token = session['access_token']
+        # Quit the driver when authenticated
         self.driver.quit()
+
+        item = f'oidc.user:https://accounts.magister.net:M6-{self.school_url}'
+        result = self.driver.execute_script(f"return window.sessionStorage.getItem('{item}')")
+        self.session = json.loads(result)
+
+    """
+    Authenticate the user
+    """
+    def authenticate(self):
+        if not path.isfile('.session'):
+            self.__login()
+        else:
+            self.__load_session()
+
+        self.access_token = self.session['access_token']
+        self.__write_session()
         return self.access_token
 
