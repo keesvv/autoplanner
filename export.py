82a707340064092389272697d7689b83e881d980
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-04-10 20:31:00 +0200

    Enhance the exporter

--- export.py
+++ export.py
@@ -2,13 +2,18 @@ import datetime
 import iso8601
 
 def parse_item(item):
-    #date_obj = datetime.datetime.strptime(item['Start'], '%Y-%m-%dT%H:%M:%SZ')
     date_obj = iso8601.parse_date(item['Start'])
     friendly_date = date_obj.strftime('%a, %d %b %Y %H:%M:%S')
+    contents = item['Inhoud']
+
+    # Set contents to '' if None
+    if contents == None:
+        contents = ''
 
     return item['Vakken'][0]['Naam'] + ';'\
-        + item['Inhoud'] + ';'\
-        + friendly_date + ';'
+        + contents + ';'\
+        + friendly_date + ';'\
+        + '\n'
 
 class Exporter:
     def __init__(self, data):
@@ -24,8 +29,12 @@ class Exporter:
         contents = map(parse_item, self.data)
 
         file = open('roster.csv', 'w')
+
+        # Write the columns to the roster file
         file.writelines(map(lambda c: c + ';', columns))
         file.write('\n')
+
+        # Write the roster contents to the roster file
         file.writelines(contents)
         file.close()
 
