f69546abffc98649ca77019d6f4d5a957d6d91a6
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-04-10 23:38:07 +0200

    Refactoring

--- src/api.py
+++ src/api.py
@@ -1,14 +1,13 @@
 import requests
 
-session = requests.Session()
-
 class API:
     def __init__(self, auth_provider):
         self.auth_provider = auth_provider
-        session.headers['Authorization'] = f'Bearer {auth_provider.access_token}'
+        self.session = requests.Session()
+        self.session.headers['Authorization'] = f'Bearer {auth_provider.access_token}'
 
     def request(self, url):
-        result = session.get(
+        result = self.session.get(
                 'https://' + self.auth_provider.school_url + url
         )
         
