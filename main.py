92b9954e7ad5f3388c1f7691c7f82f1039f8480d
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-04-10 20:31:31 +0200

    Got basic functionality working

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
 import config
+from api import API
 from auth import AuthProvider
+from export import Exporter
 
+# Instantiate a Authentication Provider
 prov = AuthProvider(config.SCHOOL_URL)
 
+# Authenticate the user
 prov.authenticate()
+
+api = API(prov)
+
+# Store the roster inside of a variable
+roster = api.get_roster()
+
+exporter = Exporter(roster)
+
+# Export the roster to a CSV format
+exporter.export_csv()
 
